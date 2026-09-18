77351c7e02d62a880d646a98e568c97306f8e5ee
Ivan <154084167+hrytsev@users.noreply.github.com>
2026-09-18 14:21:26 +0300

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -4,22 +4,20 @@ from numpy.typing import NDArray
 
 class Solution:
 
-    def binary_cross_entropy(self, y_true, y_pred) -> float:
-    # y_true: true labels (0 or 1)
-    # y_pred: predicted probabilities
-    # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
-    # return round(your_answer, 4)
-        e=1e-7
-        logits_sum=sum(y_true*np.log(y_pred+e)+(1-y_true+e)*np.log(1-y_pred))
-        res=-logits_sum/len(y_true)
-        return np.round(res,4)
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: clip y_pred to [1e-7, 1 - 1e-7] to avoid log(0)
+        # return round(your_answer, 4)
+        y_pred=np.clip(y_pred,1e-7,1-1e-7)
+        bce_loss=-np.sum(y_true*np.log(y_pred) + (1-y_true) * np.log(1-y_pred )  )/len(y_true)
+        return np.round(bce_loss,4)
 
     def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
         # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
         # y_pred: predicted probabilities (shape: n_samples x n_classes)
-        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # Hint: clip y_pred to [1e-7, 1 - 1e-7] to avoid log(0)
         # return round(your_answer, 4)
-        e=1e-7
-        logits_sum=np.sum(y_true*np.log(y_pred))
-        res=-logits_sum/len(y_true)
-        return np.round(res,4)
+        y_pred=np.clip(y_pred,1e-7,1-1e-7)
+        ce_loss=-np.sum(np.sum(y_true*np.log(y_pred)))/len(y_true)
+        return np.round(ce_loss,4)
